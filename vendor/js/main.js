ce25a59147fa0ce4b4286989f9cf20ef256f1108
Md-Hasan-Fardous <frworkbd@gmail.com>
2015-02-18 21:57:45 +0600

    Js Css files moved to vendor
    Js Css files moved to vendor

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
 	$('#nav').slicknav();
+	
+	var owl = $("#owl-demo");
+     
+    owl.owlCarousel({
+		items : 4, //10 items above 1000px browser width
+		itemsDesktop : [1000,3], //5 items between 1000px and 901px
+		itemsDesktopSmall : [900,3], // betweem 900px and 601px
+		itemsTablet: [600,2], //2 items between 600 and 0
+		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+		autoPlay: 3000,
+		pagination: false
+    });
+     
+    // Custom Navigation Events
+    $(".next").click(function(){
+    owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+    owl.trigger('owl.prev');
+    })
+    $(".play").click(function(){
+    owl.trigger('owl.play',2000); //owl.play event accept autoPlay speed as second parameter
+    })
+    $(".stop").click(function(){
+    owl.trigger('owl.stop');
+    })
 });

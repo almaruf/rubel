c2120b77317ae888ec004be3973658e52672fc7c
Md-Hasan-Fardous <frworkbd@gmail.com>
2015-02-24 21:46:47 +0600

    Update footer and style's file
    Update footer and style's file

--- blood/footer.php
+++ blood/footer.php
@@ -1,8 +1,16 @@
 		<section class="footer_area">		
 			<div class="container">
 				<div class="row">				
-					<div class="footer copyright">
-						<p>&copy; ডিজাইন এবং ডেভেলপ করেছে <a href="">হাসান ফেরদৌস রুবেল</a></p>
+					<div class="pull-left col-lg-6 col-md-6 col-sm-6">
+						<ul class="pull-left footer_menu">
+							<li><a href="">Contact Us</a></li>
+							<li><a href="">Help</a></li>
+							<li><a href="">About Us</a></li>
+						</ul>
+						<p class="pull-right"><a href=""><b>Home Page</b></a></p>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						<p class="pull-right">&copy; ডিজাইন এবং ডেভেলপ করেছে <a href="">Hasan Fardous Rubel</a></p>
 					</div>
 				</div>
 			</div>

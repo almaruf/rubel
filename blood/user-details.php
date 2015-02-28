c7291e242907f4132213ae47a6dd8e6dd1453b52
Md-Hasan-Fardous <frworkbd@gmail.com>
2015-02-28 22:24:03 +0600

    User details page created
    User details page created

---
+++ blood/user-details.php
@@ -0,0 +1,37 @@
+<?php 
+	include('header.php');
+?>
+	<div class="container">
+		<div class="row">
+			<div class="users">
+				<div class="col-lg-7 col-md-7 col-sm-7 user_info">
+					<h3 class="name">স্বাগতম "মোঃ হাসান ফেরদৌস রুবেল"</h3>
+					<p class="users_info">
+						আপনার রক্তের গ্রুপ : AB+<br />
+						আপনার মোবাইল নম্বর : 0184 0000 000<br />
+						দ্বিতীয় মোবাইল নম্বর/ইমেইল এড্রেস (অ্যাকাউন্ট সিকিউরিটির জন্য) : 0170 0000 000<br />
+						জেলা : ঢাকা<br />
+						থানা : কেরানীগঞ্জ<br />
+						ইউনিয়ন/সিটি কর্পোরেশন : ঢাকা<br />
+						ওয়ার্ড : ১<br />
+						জন্ম তারিখ : ১৯শে মার্চ ১৯৮৭<br />
+						ওজন : ৬৩<br />  
+						শেষ রক্ত দিয়েছেন : ২০শে ফেব্রুয়ারী ২০১৫<br /><br />
+					</p>
+					<a href="" class="btn btn-info">বিবরন পরিবর্তন করুন</a>
+					<a href="" class="btn btn-info">পাসওয়ার্ড পরিবর্তন করুন</a>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 user_photo">
+					<img src="img/users/user1.jpg" class="img-responsive" alt="" />
+					<br />
+					<a href="" class="btn btn-info">ছবি পরিবর্তন করুন</a>
+				</div>
+			</div>
+			
+			<div class="users_history">
+				
+			</div>
+		</div>
+	</div>			
+	
+<?php include('footer.php');?>

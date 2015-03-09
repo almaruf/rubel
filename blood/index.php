c27470a31c8d4b50d533aea1cb265c445d6813a2
Md-Hasan-Fardous <frworkbd@gmail.com>
2015-03-09 20:45:59 +0600

    Index file updated
    Index file updated

--- blood/index.php
+++ blood/index.php
@@ -1,5 +1,125 @@
 <?php 
 	include('header.php');
+	//include('address-list.php');
+	include('config.php');
+	if(isset($_POST['search_donor'])){
+		$name = $_POST['name'];
+		$mobile = $_POST['mobile'];
+		$seeker_number = $_POST['seeker_number'];
+		if(isset ( $_POST['donate_blood'] )) {
+			$donate_blood = $_POST['donate_blood'];
+		}
+		$district = $_POST['district'];
+		$state = $_POST['state'];
+		$seeker_union = $_POST['seeker_union'];
+		$seeker_word = $_POST['seeker_word'];
+		$verification_code = mt_rand(100000, 999999);	//send this code to mobile phone
+		if( empty($name) || empty($mobile) || empty($seeker_number) || empty($donate_blood) || empty($district) || empty($state) || empty($seeker_union) || empty($seeker_word) ){
+			$search_donor_error = "You can not leave any field empty. Please re enter your details.";
+		}else{
+			try{
+				if(!is_numeric($mobile)){
+					throw new Exception("Please enter your mobile number correctly.");
+				}
+				
+				$statement = $db->prepare("INSERT INTO blood_seeker VALUES(?,?,?,?,?,?,?,?,?,?)");
+				if( $statement->execute(array('', $name, $mobile, $seeker_number, $donate_blood, $district, $state, $seeker_union, $seeker_word, $verification_code)) ){
+					$id = $db->lastInsertId();
+					$success_mesg = "Your information submitted successfully.";
+					if( $donate_blood == 'yes' ){
+						header('refresh:3; url=verification.php?id='.$id.'&donate=1');
+					}elseif( $donate_blood == 'no' ){
+						header('refresh:3; url=verification.php?id='.$id.'&donate=0');
+					}
+				}else{
+					$search_donor_error = "Sorry! Your information did not submit.";
+				}
+			}
+			catch (Exception $e) {
+				$search_donor_error = $e->getMessage();
+			}
+		}
+	}
+	
+	
+	if( isset($_POST['add_donor_list']) ){
+		try{
+			if ( empty( $_POST['name'] ) ) {
+				throw new Exception ("Name can be blank.");
+			}
+			if ( empty( $_POST['blood_group'] ) ) {
+				throw new Exception ("Blood group can be blank.");
+			}
+			if ( empty( $_POST['mobile'] ) ) {
+				throw new Exception ("Mobile can be blank.");
+			}
+			if ( empty( $_POST['secondary_mobile'] ) ) {
+				throw new Exception ("Secondary mobile can be blank.");
+			}
+			if ( empty( $_POST['district'] ) ) {
+				throw new Exception ("District can be blank.");
+			}
+			if ( empty( $_POST['state'] ) ) {
+				throw new Exception ("State can be blank.");
+			}
+			if ( empty( $_POST['donor_union'] ) ) {
+				throw new Exception ("Union can be blank.");
+			}
+			if ( empty( $_POST['word'] ) ) {
+				throw new Exception ("Word can be blank.");
+			}
+			if ( empty( $_POST['birth_year'] ) ) {
+				throw new Exception ("Birth year can be blank.");
+			}
+			if ( empty( $_POST['birth_month'] ) ) {
+				throw new Exception ("Birth month can be blank.");
+			}
+			if ( empty( $_POST['birth_day'] ) ) {
+				throw new Exception ("Birth day can be blank.");
+			}
+			if ( empty( $_POST['weight'] ) ) {
+				throw new Exception ("Weight can be blank.");
+			}
+			if ( empty( $_POST['donation_year'] ) ) {
+				throw new Exception ("Donation year can be blank.");
+			}
+			if ( empty( $_POST['donation_month'] ) ) {
+				throw new Exception ("Donation month can be blank.");
+			}
+			if ( empty( $_POST['donation_day'] ) ) {
+				throw new Exception ("Donation day can be blank.");
+			}
+			
+			$name = $_POST['name'];
+			$blood_group = $_POST['blood_group'];
+			$mobile = $_POST['mobile'];
+			$secondary_mobile = $_POST['secondary_mobile'];
+			$district = $_POST['district'];
+			$state = $_POST['state'];
+			$donor_union = $_POST['donor_union'];
+			$word = $_POST['word'];
+			$birthY[] = $_POST['birth_year'];
+			$birthY[] = $_POST['birth_month'];
+			$birthY[] = $_POST['birth_day'];
+			$dob_donor = implode('-', $birthY);
+			$weight = $_POST['weight'];
+			$don[] = $_POST['donation_year'];
+			$don[] = $_POST['donation_month'];
+			$don[] = $_POST['donation_day'];
+			$last_donation = implode('-', $don);
+			
+			$statement = $db->prepare("INSERT INTO blood_donor VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			if ( $statement->execute(array('',$name,$blood_group,$mobile,$secondary_mobile,$district,$state,$donor_union,$word,$dob_donor,$weight,$last_donation,'no-image.gif')) ) {
+				$added_donor = "Added you to donor list successfully.";
+				header('refresh:3;url=index.php');
+			}else{
+				$error_message = "Something went wrong, you did not add to donor list.";
+			}
+		}
+		catch (Exception $e) {
+			$error_message = $e->getMessage();
+		}
+	}
 ?>
 	<div class="container">
 		<div class="row">
@@ -7,7 +127,23 @@
 				<p class="title"><b>রক্ত দরকার</b></p>
 				<hr />
 				<br />
-				<form method="post" action="verification.php">
+				<?php
+					if(isset($search_donor_error)){
+				?>	
+				<div class="alert alert-danger alert-dismissible fade in" role="alert">
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				  <strong>Warning!</strong> <?php echo $search_donor_error;?>
+				</div>
+				<?php
+					}
+					if(isset($success_mesg)){
+				?>	
+				<div class="alert alert-success alert-dismissible" role="alert">
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				  <strong><i class="fa fa-check-circle"></i> </strong> <?php echo $success_mesg;?>
+				</div>
+				<?php } ?>
+				<form method="post">
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="name">আপনার নাম :</label>
 					<input type="text" name="name" class="col-lg-6 col-md-6 col-sm-6 form-control" id="name" placeholder="আপনার নাম">
@@ -18,7 +154,7 @@
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="donornumber">আপনার ডোনার নম্বর (যদি থাকে) :</label>
-					<input type="text" name="donor_number" class="col-lg-6 col-md-6 col-sm-6 form-control" id="donornumber" placeholder="আপনার ডোনার নম্বর (যদি থাকে)">
+					<input type="text" name="seeker_number" class="col-lg-6 col-md-6 col-sm-6 form-control" id="donornumber" placeholder="আপনার ডোনার নম্বর (যদি থাকে)">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="donatefuture">ভবিষ্যতে আপনিও অন্যের দরকারে রক্ত দিতে চান :</label>
@@ -47,7 +183,7 @@
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">ইউনিয়ন/সিটি কর্পোরেশন :</label>
-					<select class="form-control" name="union">
+					<select class="form-control" name="seeker_union">
 						<option value="">পছন্দ করুন</option>
 						<option value="dhaka">ঢাকা</option>
 						<option value="sylhet">সিলেট</option>
@@ -56,8 +192,8 @@
 					</select>
 				  </div>
 				  <div class="form-group clearfix">
-					<label class="col-lg-6 col-md-6 col-sm-6" for="word">আপনার ওয়ার্ড :</label>
-					<input type="text" name="word" class="col-lg-6 col-md-6 col-sm-6 form-control" id="word" placeholder="আপনার ওয়ার্ড">
+					<label class="col-lg-6 col-md-6 col-sm-6" for="seeker_word">আপনার ওয়ার্ড :</label>
+					<input type="text" name="seeker_word" class="col-lg-6 col-md-6 col-sm-6 form-control" id="word" placeholder="আপনার ওয়ার্ড">
 				  </div>
 				  <button type="submit" name="search_donor" class="btn btn-info pull-right">খোঁজ করুন</button>
 				</form>
@@ -66,10 +202,26 @@
 				<p class="title"><b>রক্ত দিতে চাই</b></p>
 				<hr />
 				<br />
-				<form method="post" action="verification.php">
+				<?php
+					if(isset($error_message)){
+				?>	
+				<div class="alert alert-danger alert-dismissible fade in" role="alert">
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				  <strong>Warning!</strong> <?php echo $error_message;?>
+				</div>
+				<?php
+					}
+					if(isset($added_donor)){
+				?>	
+				<div class="alert alert-success alert-dismissible" role="alert">
+				  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				  <strong><i class="fa fa-check-circle"></i> </strong> <?php echo $added_donor;?>
+				</div>
+				<?php } ?>
+				<form method="post" action="">
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="name">আপনার নাম :</label>
-					<input type="text" class="col-lg-6 col-md-6 col-sm-6 form-control" id="name" placeholder="আপনার নাম">
+					<input type="text" name="name" class="col-lg-6 col-md-6 col-sm-6 form-control" placeholder="আপনার নাম">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">আপনার রক্তের গ্রুপ :</label>
@@ -87,49 +239,71 @@
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="donornumber">আপনার মোবাইল :</label>
-					<input type="text" class="col-lg-6 col-md-6 col-sm-6 form-control" id="donornumber" placeholder="আপনার ডোনার নম্বর (যদি থাকে)">
+					<input type="text" name="mobile" class="col-lg-6 col-md-6 col-sm-6 form-control" placeholder="আপনার ডোনার নম্বর (যদি থাকে)">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="donornumber">দ্বিতীয় মোবাইল নম্বর/ইমেইল অ্যাড্রেস (অ্যাকাউন্ট সিকিউরিটির জন্য) :</label>
-					<input type="text" class="col-lg-6 col-md-6 col-sm-6 form-control" id="donornumber" placeholder="someone@someone.com" style="margin-top: 10px;">
+					<input type="text" name="secondary_mobile" class="col-lg-6 col-md-6 col-sm-6 form-control" placeholder="someone@someone.com" style="margin-top: 10px;">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">আপনার জেলা :</label>
-					<select class="form-control">
+					<select class="form-control" name="district">
 						<option value="">আপনার জেলা পছন্দ করুন</option>
-						<option value="dhaka">ঢাকা</option>
-						<option value="sylhet">সিলেট</option>
-						<option value="rongpur">রংপুর</option>
-						<option value="bogura">বগুড়া</option>
+						<?php
+							$divisions = include_once('address-list.php');
+							foreach($divisions as $divisionName => $divisionDetails) {
+								echo '<optgroup label="'.$divisionName.'">';
+								foreach($divisionDetails['districts'] as $districtName => $districtDetails) {
+									echo '<option value="'.$districtName.'">'.$districtDetails['name_bn'].'</option>';
+								}
+								echo '</optgroup>';
+							}
+						?>
 					</select>
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">আপনার থানা :</label>
-					<select class="form-control">
+					<select class="form-control" name="state">
 						<option value="">আপনার থানা পছন্দ করুন</option>
-						<option value="dhaka">ঢাকা</option>
-						<option value="sylhet">সিলেট</option>
-						<option value="rongpur">রংপুর</option>
-						<option value="bogura">বগুড়া</option>
+						<?php
+							foreach($divisions as $divisionName => $divisionDetails) {
+							  foreach($divisionDetails['districts'] as $districtName => $districtDetails) {
+								echo '<optgroup label="'.$districtName.'">';
+								 foreach($districtDetails['thanas'] as $thanaName => $thanaDetails) {
+									echo '<option value="'.$thanaName.'">'.$thanaDetails['name_bn'].'</option>';
+								  }
+								echo '</optgroup>';  
+							  }
+
+							}
+						?>
 					</select>
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">ইউনিয়ন/সিটি কর্পোরেশন :</label>
-					<select class="form-control">
+					<select class="form-control" name="donor_union">
 						<option value="">পছন্দ করুন</option>
-						<option value="dhaka">ঢাকা</option>
-						<option value="sylhet">সিলেট</option>
-						<option value="rongpur">রংপুর</option>
-						<option value="bogura">বগুড়া</option>
+						<?php
+							foreach($divisions as $divisionName => $divisionDetails) {
+							  foreach($divisionDetails['districts'] as $districtName => $districtDetails) {
+								echo '<optgroup label="'.$districtName.'">';
+								 foreach($districtDetails['thanas'] as $thanaName => $thanaDetails) {
+									echo '<option value="'.$thanaName.'">'.$thanaDetails['name_bn'].'</option>';
+								  }
+								echo '</optgroup>';  
+							  }
+
+							}
+						?>
 					</select>
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="word">আপনার ওয়ার্ড :</label>
-					<input type="text" class="col-lg-6 col-md-6 col-sm-6 form-control" id="word" placeholder="আপনার ওয়ার্ড">
+					<input type="text" name="word" class="col-lg-6 col-md-6 col-sm-6 form-control" placeholder="আপনার ওয়ার্ড">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">জন্ম তারিখ :</label>
-					<select class="form-control dob">
+					<select class="form-control dob" name="birth_year">
 						<option value="">বছর</option>
 						<?php
 							for($yr = 0; $yr <= 115; $yr++){
@@ -144,7 +318,7 @@
 							}
 						?>
 					</select>
-					<select class="form-control dob">
+					<select class="form-control dob" name="birth_month">
 						<option value="">মাস</option>
 						<?php
 							for($mnth = 1; $mnth <= 12; $mnth++){
@@ -152,7 +326,7 @@
 							}
 						?>
 					</select>
-					<select class="form-control dob">
+					<select class="form-control dob" name="birth_day">
 						<option value="">দিন</option>
 						<?php
 							for($dy = 1; $dy <= 31; $dy++){
@@ -163,11 +337,11 @@
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="word">আপনার ওজন (কেজি) :</label>
-					<input type="text" class="col-lg-6 col-md-6 col-sm-6 form-control" id="word" placeholder="আপনার ওজন">
+					<input type="text" name="weight" class="col-lg-6 col-md-6 col-sm-6 form-control" placeholder="আপনার ওজন">
 				  </div>
 				  <div class="form-group clearfix">
 					<label class="col-lg-6 col-md-6 col-sm-6" for="district">শেষ কবে রক্ত দিয়েছেন :</label>
-					<select class="form-control dob">
+					<select class="form-control dob" name="donation_year">
 						<option value="">বছর</option>
 						<?php
 						for($yr = 0; $yr <= 5; $yr++){
@@ -175,7 +349,7 @@
 						}
 						?>
 					</select>
-					<select class="form-control dob">
+					<select class="form-control dob" name="donation_month">
 						<option value="">মাস</option>
 						<?php
 							for($mnth = 1; $mnth <= 12; $mnth++){
@@ -183,7 +357,7 @@
 							}
 						?>
 					</select>
-					<select class="form-control dob">
+					<select class="form-control dob" name="donation_day">
 						<option value="">দিন</option>
 						<?php
 							for($dy = 1; $dy <= 31; $dy++){
@@ -192,7 +366,7 @@
 						?>
 					</select>
 				  </div>
-				  <button type="submit" name="add_donor_list" class="btn btn-info pull-right">তালিকাভুক্ত হোন</button>
+				  <button type="submit" name="add_donor_list" class="btn btn-info pull-right">তালিকাভূক্ত হোন</button>
 				</form>
 			</div>
 		</div>

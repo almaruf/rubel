bfb3a4c063c093ef9a5840b07114eade29f5d42b
Md Abdullah Al Maruf <maruf.sylhet@gmail.com>
2015-03-09 10:33:21 +0000

    updated to use db array from dbconfig.php

--- blood/config.php
+++ blood/config.php
@@ -1,15 +1,14 @@
 <?php
-$dbhost = 'localhost';
-$dbname = 'blood_donation';
-$dbuser = 'root';
-$dbpass = '';
+$dbArray = include_once('../dbconfig.php');
 
-try{
+$dbhost = $dbArray['host'];
+$dbname = $dbArray['dbname'];
+$dbuser = $dbArray['username'];
+$dbpass = $dbArray['password']
+
+try {
  $db = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser,$dbpass);
  $db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-
-catch(PDOException $e) {
+} catch(PDOException $e) {
  echo "Connection Error : ".$e->getMessage();
 }
-?>
